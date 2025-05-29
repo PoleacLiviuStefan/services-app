c9c420436e9e21c5a25e8a690c6edffabf527019
Poleac Liviu Stefan <liviu-stefan.poleac@s.unibuc.ro>
2025-05-29 03:14:41 +0300

    go back to reset page

--- src/app/resetare-parola/page.tsx
+++ src/app/resetare-parola/page.tsx
@@ -1,15 +1,21 @@
-// app/verificare-mail/page.tsx
-'use client'
-export const dynamic = 'force-dynamic'
+// app/resetare-parola/page.tsx
+import React from 'react'
+import ResetPasswordForm from '@/components/ResetPasswordForm'
+import NewPasswordForm from '@/components/NewPasswordForm'
 
-import { Suspense } from 'react'
-import VerifyMailContent from '../verificare-mail/page'
+interface PageProps {
+  searchParams: { token?: string }
+}
 
+export default async function Page({ searchParams }: PageProps) {
+   const token = searchParams.token
 
-export default function Page() {
   return (
-    <Suspense fallback={<p className="text-center p-4">Se încarcă…</p>}>
-      <VerifyMailContent />
-    </Suspense>
+    <div className="flex justify-center items-center min-h-screen w-full bg-gradient-to-t from-primaryColor to-secondaryColor">
+      {token
+        ? <NewPasswordForm token={token} />
+        : <ResetPasswordForm />
+      }
+    </div>
   )
 }
